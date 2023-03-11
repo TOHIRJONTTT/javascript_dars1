a2f72f66e9239c876c4abccecce60008a79f31b8
TOHIRJONTTT <tohirjon009988@gmail.com>
2023-03-11 18:23:40 +0500

    java

---
+++ js/index.js
@@ -0,0 +1,20 @@
+ 
+var name = prompt ("1.What is your name?")
+var surname = prompt("2.What is your surname?")
+var age = prompt("3.How old are you?")
+var profession = prompt("4.Work/Study?")
+var region = prompt("5.Where do you live?")
+var language = prompt("6.How many languages do you know?")
+var phone = prompt("7.Your phone number?")
+var postCode = prompt("8.What is your post code?")
+var email = prompt("9.Your email address?")
+var gender = confirm("10.Are you male?")
+var confirmation = confirm("Are you sure to submit your personal information?")
+
+
+// alert("Are you sure to submit your personal information?", confirmation)
+   alert("1.NAME:" +  name + " 2.SURNAME:" + surname + " AGE:" + age + " PROFESSION:" + profession + " REGION:" + 
+   region + " LANGUAGE:" + language + " PHONE:" + phone + " POSTCODE:" + postCode + " EMAIL:" + email + " GENDER:" + 
+   gender + " CONFIRMATION:" + confirmation)
+ 
+   
